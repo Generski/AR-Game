3b89e8c0ee0239c7ee10a5c971ed759a99cb0558
Generski <generface90@gmail.com>
2020-11-07 20:34:36 +0000

    Ball game added

---
+++ AR Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private SphereCollider collider;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        collider = GetComponent<SphereCollider>();
+
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if(collider.enabled == true)
+        {
+            rb.useGravity = true;
+        }
+    }
+}
--- AR Game/Assets/Scripts/Player/PlayerMovement.cs
+++ AR Game/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,32 +7,27 @@ public class PlayerMovement : MonoBehaviour
 {
     private PlayerControls controls;
 
+    private Rigidbody rb;
+
     [SerializeField] private float moveSpeed = 10f;
     private Vector2 move;
-
-    private CharacterController characterController;
 
     private void Awake()
     {
         controls = new PlayerControls();
+        rb = GetComponent<Rigidbody>();
+    }
 
-        characterController = GetComponent<CharacterController>();
+    private void Update()
+    {
+        move = controls.Gameplay.Move.ReadValue<Vector2>();
     }
 
     private void FixedUpdate()
     {
-        move = controls.Gameplay.Move.ReadValue<Vector2>();
-
         Vector3 movement = (move.y * transform.forward) + (move.x * transform.right);
 
-        characterController.Move(movement * moveSpeed * Time.deltaTime);
-
-        controls.Gameplay.ResetPos.started += context => ResetPos();
-    }
-
-    private void ResetPos()
-    {
-        transform.position = Vector3.zero;
+        rb.velocity = transform.position + movement;
     }
 
     private void OnEnable()
